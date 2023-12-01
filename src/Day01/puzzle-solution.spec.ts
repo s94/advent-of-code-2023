99130fa06c47919f011583c333cdae805c4f6427
Adam Slater <mr.slater.94@gmail.com>
2023-12-01 15:16:31 +0000

    Refactored solution for Day 01 Part 1.

--- src/Day01/puzzle-solution.spec.ts
+++ src/Day01/puzzle-solution.spec.ts
@@ -8,22 +8,18 @@ describe("Day 01", () => {
 	it("Part 1", () => {
 		let calibrationValues: number[] = [];
 		fileContents.forEach((line) => {
-			let lineCalibrationValue: number = 0;
 			let temp: string = "";
-			for (let index = 0; index < line.length; index++) {
-				const character: string = line[index];
-				let characterAsNumber: number = +character;
-				if (isNaN(characterAsNumber)) continue;
-				temp += character;
+			for (let i = 0; i < line.length; i++) {
+				if (isNaN(Number(line[i]))) continue;
+				temp += line[i];
 			}
-			lineCalibrationValue = +(`${temp[0]}${temp[temp.length - 1]}`);
-			calibrationValues.push(lineCalibrationValue);
+			calibrationValues.push(Number(`${temp[0]}${temp[temp.length - 1]}`));
 		});
 		let calibrationValuesSum: number = 0;
-		for (let index = 0; index < calibrationValues.length - 1; index++) {
-			const val: number = calibrationValues[index];
-			calibrationValuesSum += val;
-		}
+		calibrationValues.forEach((n: number) => {
+			if (!isNaN(n))
+				calibrationValuesSum += n;
+		});
 		assert.equal(calibrationValuesSum, 55386);
 	});
 });
