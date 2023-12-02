0d3e70f66f73ab077cfb7b2809a0fe62b9f2d2c6
Adam <mr.slater.94@gmail.com>
2023-12-02 21:07:08 +0000

    Added Day02 Part 1 Solution.

---
+++ src/Day02/puzzle-solution.test.ts
@@ -0,0 +1,68 @@
+import * as fs from 'fs';
+import { assert } from 'chai';
+
+const file: string = fs.readFileSync("./src/Day02/puzzle-input.txt", "utf-8");
+const fileContents: string[] = file.split(/\r?\n/);
+
+describe("Day 02", () => {
+	it("Part 1", () => {
+		let idSum: number = 0;
+		fileContents.forEach((line: string) => {
+			let game: CubeGame = new CubeGame(line);
+			idSum = game.maxRedCubes > 12 || game.maxGreenCubes > 13 || game.maxBlueCubes > 14 ? idSum : idSum + game.id;
+		});
+		assert.equal(idSum, 2061);
+	});
+});
+
+class CubeGame {
+	id: number;
+	maxRedCubes: number = 0;
+	maxGreenCubes: number = 0;
+	maxBlueCubes: number = 0;
+
+	constructor(game: string)
+	{
+		let gameInfo: string[] = game.split(":");
+		let id: string = gameInfo[0].replace("Game ", "");
+		this.id = Number(id);
+		this.processSets(gameInfo[1].split(";"));
+	}
+
+	private processSets(sets: string[]): void {
+		sets.forEach((set: string) => this.processCubes(set.split(",")));
+	}
+
+	private processCubes(cubes: string[]): void {
+		cubes.forEach((cube: string) => {
+			let val: number = parseInt(cube);
+			let colour: CubeColour = this.getColour(cube);
+
+			switch (colour) {
+				case CubeColour.red:
+					this.maxRedCubes = this.maxRedCubes < val ? val : this.maxRedCubes;
+					break;
+				case CubeColour.green:
+					this.maxGreenCubes = this.maxGreenCubes < val ? val : this.maxGreenCubes;
+					break;
+				case CubeColour.blue:
+					this.maxBlueCubes = this.maxBlueCubes < val ? val : this.maxBlueCubes;
+					break;
+				default:
+					break;
+			}
+		});
+	}
+
+	private getColour(cube: string): CubeColour {
+		if (cube.includes("red")) return CubeColour.red;
+		if (cube.includes("green")) return CubeColour.green;
+		if (cube.includes("blue")) return CubeColour.blue;
+	}
+}
+
+enum CubeColour {
+	red,
+	green,
+	blue
+}
