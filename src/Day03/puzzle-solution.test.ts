0e4ad1022676eed265d541beb532534cb51d2e9c
Adam <mr.slater.94@gmail.com>
2023-12-04 22:54:28 +0000

    Initial commit.

---
+++ src/Day03/puzzle-solution.test.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+import { assert } from 'chai';
+
+const file: string = fs.readFileSync("./src/Day03/puzzle-input.txt", "utf-8");
+const fileContents: string[] = file.split(/\r?\n/);
+
+describe("Day 03", () => {
+	it("Part 1", () => {
+		
+	});
+});
